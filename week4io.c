e621c6f6454a6882cf99b28fe6490bb1905b1e25
Nikhilapriya <60135587+Nikhilapriya@users.noreply.github.com>
2020-02-08 23:57:47 +0530

    week4

---
+++ week4io.c
@@ -0,0 +1,21 @@
+for BT-1
+
+    inorder(left,root,right) : DBGEHACF
+    preorder(root,left,right) : ABDEGHCF
+    postorder(left,right,root) : DGHEBFCA
+    levelorder: ABCDEFGH
+
+for BT-2
+
+    inorder(left,root,right) : HDBEAFCG
+    preorder(root,left,right) : ABDHECFG
+    postorder(left,right,root) : HDEBFGCA
+    levelorder: ABCDEFGH
+
+
+
+    HASHTABLES:
+        Hashing is an important Data Structure which is designed to use a special function called
+        the Hash function which is used to map a given value with a particular key for faster
+        access of elements. The efficiency of mapping depends of the efficiency of the hash
+        function used.
